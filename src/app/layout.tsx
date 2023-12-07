d57ad590b9aa3944327f2664a42c057d0c6639f6
mfitrahrmd <tgcfitrah26@gmail.com>
2023-12-07 17:34:28 +0700

    setup prisma & mui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
+import ThemeRegistry from '@/mui/ThemeRegistry'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+// import { Inter } from 'next/font/google'
+// import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/* <body className={inter.className}>{children}</body> */}
+      <body>
+        <ThemeRegistry options={{ key: 'mui' }}>{children}</ThemeRegistry>
+      </body>
     </html>
   )
 }
